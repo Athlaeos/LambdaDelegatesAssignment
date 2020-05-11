d740ce8aa0e46ba63f2eddff5358f53cc82339f2
jobko <jobkoppenol@gmail.com>
2020-05-11 15:52:51 +0200

    practicum2 commit

---
+++ practicum2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace practicum2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random rnd = new Random();
+            num1Text.Text = rnd.Next(10).ToString();
+            num2Text.Text = rnd.Next(10).ToString();
+            num3Text.Text = rnd.Next(10).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(num1Text.Text);
+            int num2 = Int32.Parse(num2Text.Text);
+            int num3 = Int32.Parse(num3Text.Text);
+
+            String output = MethodRunner.RunAllMethods(num1,num2,num3);
+            methodOutput.Text = output;
+
+            output = LambdaRunner.RunAllMethods(num1,num2,num3);
+            lambdaOutput.Text = output;
+            String[] splittextlambda = lambdaOutput.Text.Split(new string[] { "\n" }, StringSplitOptions.None);
+            String[] splittextmethod = methodOutput.Text.Split(new string[] { "\n" }, StringSplitOptions.None);
+            bool ok = true;
+            for (int i = 0; i < splittextmethod.Length - 1; i++){
+                if (splittextlambda[i].Split(new string[] { " = " }, StringSplitOptions.None)[1] != splittextmethod[i].Split(new string[] { " = " }, StringSplitOptions.None)[1]){
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok){
+                System.Windows.Forms.MessageBox.Show("Output ok!");
+            } else {
+                System.Windows.Forms.MessageBox.Show("Output not ok!");
+            }
+
+        }
+    }
+}
---
+++ practicum2/LambdaRunner.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace practicum2
+{
+    class LambdaRunner
+    {
+        public static String RunAllMethods(int num1, int num2, int num3)
+        {
+            StringBuilder output = new StringBuilder();
+
+            //TimesThree
+            Func<int, int> timesThree = x => 3 * x;
+            output.AppendFormat("TimesThree({0}) = {1}\n", num1, timesThree(num1));
+
+            //AddNumbers
+            Func<int, int, int, int> addUp = (a, b, c) => a + b + c;
+            output.AppendFormat("AddNumbers({0},{1},{2}) = {3}\n", num1, num2,num3, addUp(num1,num2,num3));
+
+            //IsEven
+            Func<int, bool> isEven = x => x % 2 == 0;
+            output.AppendFormat("IsEven({0}) = {1}\n", num1, isEven(num1));
+
+            //Num2String
+            Func<int, string> convert = num2string;
+            output.AppendFormat("Num2String({0}) = {1}\n", num1, convert(num1));
+
+            //InBetween
+            Func<int, int, int, bool> inBetween = (x, y, z) => (x < y && y < z) || (z < y && y < x);
+            output.AppendFormat("InBetween({0},{1},{2}) = {3}\n", num1, num2, num3, inBetween(num1,num2,num3));
+
+            //ResetName
+            Predicate<Person> resetName = ResetName;
+            Person p = new Person() {Name = "Dirk"};
+            output.AppendFormat("ResetName, daarna (Name == null) = {0}\n",resetName(p));
+
+            return output.ToString();
+        }
+
+        private static bool ResetName(Person p){
+            p.Name = null;
+            if (p.Name == null){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private static String num2string(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    return "zero"; break;
+                case 1:
+                    return "one"; break;
+                case 2:
+                    return "two"; break;
+                case 3:
+                    return "three"; break;
+                case 4:
+                    return "four"; break;
+                case 5:
+                    return "five"; break;
+                case 6:
+                    return "six"; break;
+                case 7:
+                    return "seven"; break;
+                case 8:
+                    return "eight"; break;
+                case 9:
+                    return "nine"; break;
+                default:
+                    return "undefined"; break;
+            }
+        }
+    }
+}
